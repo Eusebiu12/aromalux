7d5390ebe0ffa3ff62cb1ca103fe9a5ee252af3e
Eusebiu12 <gheorghe.paraschivu@student.upt.ro>
2026-05-24 17:21:27 +0300

    fixed fetchmyorders for my orders page

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -5,7 +5,8 @@ class ErrorHandler extends Error {
   }
 }
 
-export const errorMiddleware = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
